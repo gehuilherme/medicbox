ca6de2f657f76f02ba01d75d2af7996932559246
Guilherme Henrique <32115850+gehuilherme@users.noreply.github.com>
2018-11-24 16:55:51 -0200

    Add files via upload

--- MedicineBox.ino
+++ MedicineBox.ino
@@ -6,18 +6,19 @@ int cont = 1;
 void setup() {
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
-  pinMode(buz,OUTPUT);
+  pinMode(buz, OUTPUT);
 }
 
 void loop() {
-  tone(buz,1000,100);
+
+  // Início da contagem 1° ciclo.
+  
   delay(30000);
-  goto med1;
-  delay(10000);
-  goto med2;
+  goto ciclo1;
+
+
   
-  
-  med1:{
+  ciclo1:{
     while(cont <= 5){
       digitalWrite(led1, HIGH);
       delay(100);    
@@ -25,13 +26,19 @@ void loop() {
       delay(100);
       tone(buz,1000,150);
       delay(1000);
+      cont++;
     }
     
-  cont = 1;
+  cont = 1; // Reset do contador.
 
   }
+
+  // Início da contagem 2° ciclo.
+
+  delay(30000);
+  goto ciclo2;
   
-  med2:{
+  ciclo2:{
     while(cont <= 5){
       digitalWrite(led2, HIGH);
       delay(100);    
@@ -39,9 +46,32 @@ void loop() {
       delay(100);
       tone(buz,1000,150);
       delay(1000);
+      cont++;
     }
   }
+
+  cont = 1; // Reset do contador.
+
+
+  // Início da contagem 3° ciclo.
   
-  cont = 1;
+  delay(30000);
+  goto ciclo3;
+
+  ciclo3:{
+    while(cont <= 5){
+      digitalWrite(led1, HIGH);
+      delay(100);    
+      digitalWrite(led1,LOW);
+      delay(100);
+      tone(buz,1000,150);
+      delay(1000);
+      cont++;
+    }
+    
+  cont = 1; // Reset do contador.
+
+  }
+    
   
 }
